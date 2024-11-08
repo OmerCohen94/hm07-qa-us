41fa59c4a00e70b416a761393172a27e12f68338
Omer Cohen <omercohen94@gmail.com>
2024-11-08 15:13:16 +0200

    First attempt at project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-3a021b42-6e61-4c99-8573-cdb3f6207ac5.containerhub.tripleten-services.com/"
+    API_URL: "https://cnt-ffb8dc42-6fc2-4451-b863-e6647d2f4df9.containerhub.tripleten-services.com"
 }
